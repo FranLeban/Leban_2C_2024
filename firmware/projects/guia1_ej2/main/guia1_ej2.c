b7f32a89e3406eb227e4af157303877d8639287e
Francesca Leban <francheleban123@gmail.com>
2024-07-31 11:09:07 -0300

    Se sube resolución de ejercicio 2 de la guía 1

---
+++ firmware/projects/guia1_ej2/main/guia1_ej2.c
@@ -0,0 +1,60 @@
+/*! @mainpage guia1_ej2
+ *
+ * @section genDesc General Description
+ *
+ * Este ejercicio hace titilar los leds 1 y 2 al mantener presionada las teclas 1 y 2 correspondientemente
+ * y también hace titilar el led 3 al presionar simultáneamente las teclas 1 y 2.
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 31/07/2024 | Document creation		                         |
+ *
+ * @author Francesca Leban (francesca.leban@ingenieria.uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "switch.h"
+/*==================[macros and definitions]=================================*/
+#define CONFIG_BLINK_PERIOD 100
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	uint8_t teclas;
+	LedsInit();
+	SwitchesInit();
+    while(1)    {
+    	teclas  = SwitchesRead();
+    	switch(teclas){
+    		case SWITCH_1:
+    			LedToggle(LED_1);
+    		break;
+    		case SWITCH_2:
+    			LedToggle(LED_2);
+    		break;
+			case SWITCH_1|SWITCH_2:
+				LedToggle(LED_3);
+			break;
+    	}
+	    /*LedToggle(LED_3);*/
+		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+	}
+}
