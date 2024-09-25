7f5886faf691c33505894cfeab5edec46a1c20d0
Francesca Leban <francheleban123@gmail.com>
2024-09-25 11:03:18 -0300

    Subo documentacion generada con Doxygen del ejercicio 3 del proyecto 2

---
+++ firmware/projects/guia2_ej3/documentacion/html/search/functions_5.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['fill_0',['Fill',['../ili9341_8c.html#a1398b648b9ae023e8c33ac565fc9eaff',1,'ili9341.c']]],
+  ['funcionuartleer_1',['FuncionUartLeer',['../guia2__ej3_8c.html#a2e10000f3cc47175480716adceb56336',1,'guia2_ej3.c']]],
+  ['functecla1_2',['FuncTecla1',['../guia2__ej3_8c.html#a96fa131acb51461af657d5e317bf4638',1,'guia2_ej3.c']]],
+  ['functecla2_3',['FuncTecla2',['../guia2__ej3_8c.html#a51cc7b83c6f726c60d58f8d7c4fa222c',1,'guia2_ej3.c']]]
+];
